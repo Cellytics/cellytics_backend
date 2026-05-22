bbef752b0266fcf84407d5d6fc1044e64282a4c7
Ihimbru-k <kanihims25@gmail.com>
2026-05-22 06:48:25 +0100

    fixed backend cors

--- main.py
+++ main.py
@@ -29,7 +29,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://your-vercel-url.vercel.app",],
+allow_origin_regex=r"https://.*\.vercel\.app",
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
